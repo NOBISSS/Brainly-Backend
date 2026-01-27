2c8b2c4f9a942731c79c570440da11ef3d3df95d
Parth Chauhan <parthchauhan25125@gmail.com>
2026-01-28 01:47:18 +0530

    P

--- src/index.ts
+++ src/index.ts
@@ -21,23 +21,29 @@ const allowedOrigins = [
   "https://second-brainly-jet.vercel.app"
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
 // allow prefligh
 //app.options("/*",cors());
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
 
 app.use(express.json({limit:"10mb"}));
 app.use(express.urlencoded({extended:true}));
